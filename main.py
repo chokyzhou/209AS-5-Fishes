33e548ba4d3036b591fef98909a012dd7668001a
choky <shenmeguislb@gmail.com>
2021-10-14 17:41:46 -0700

    created new branch

--- main.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from gridworld_setup import GridWorld
-from simulator import Simulator
-
-if __name__ == "__main__":
-    grid = GridWorld()
-    print(grid.transition_probability.shape)
-    print(grid.transition_probability[4][0])
