fd5b60c67ca31d6076d84b9b19661d84e007bda8
chokyzhou <nobreakfast@foxmail.com>
2021-10-19 00:07:12 -0700

    updated proposal

--- gridWorld/utils/approximation.py
+++ gridWorld/utils/approximation.py
@@ -1,24 +1,63 @@
 import numpy as np
+import math
 
-def basis(robots, icecreams):
-    """
-    basis function phi
+def VI_Approximation(self):
+    delta = 0.1
+    N = 1
+    theta = np.array([1]*N)
+    self.initV()
+    self.showV()
 
-    @param robots: (N,) grid position of robots
-    @param icecream: (M,) grid position of icecream shops
-    return basis function (K,)
-    """
-    basis1 = [] # L1-norm of the distance between the ith agent and the icecream shop
-    basis2 = [] # L1-norm of the distance between the ith and jth agent
-    for robotA in robots:
-        basis1.append(sum([np.linalg.norm(robotA - icecream, ord=1) for icecream in icecreams]) / len(icecreams))
-        for robotB in robots:
-            if robotA != robotB:
-                basis2.append(np.linalg.norm(robotA - robotB, ord=1))
-    return basis1 + basis2
+    while True:
+        newV = {}
+        newPi = {}
+        Vth = {}
 
-def weights(N):
+        sumDiffMin = math.inf
+
+        for s in self.system.getS:
+            maxV = -math.inf
+            bestAction = None
+
+            Vth[s] = theta.T * basis(s)
+            for a in self.system.getA:
+                tempV = 0
+
+                for key, value in self.system.getP[s][str(a)].items():
+                    if value > 0 :
+                        tempV += value * (self.system.getR[key] + self.system.getGamma * Vth[s])
+                
+                if tempV > maxV:
+                    maxV = tempV
+                    bestAction = a
+            newV[s] = maxV
+            newPi[s] = bestAction
+
+        sumDiff = 0
+        for s in self.system.getS:
+            sumDiff += abs(newV[s] - Vth[s])
+
+        if sumDiffMin > sumDiff:
+            sumDiffMin = sumDiff
+            theta = 
+
+        print("=====================================================")
+        print("Diff: ", str(sumDiff))
+        self.showV()
+        if sumDiff < delta:
+            # need to ask how to generate a policy based on the value of sates
+            self.Pi = newPi
+            break
+
+
+def basis(state, targets=None):
     """
-    weighting theta
+    Calculate L1 norm between state and lists of target
+    @param state:  (1,) 
+    @return:       (2,1) basis function
     """
-    return [1] * N
+    base = []
+    for target in targets:
+        
+
+    return base
