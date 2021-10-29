0cc42a56a42c6c5bc6f5f1d9929fdc2fc02966ec
choky <shenmeguislb@gmail.com>
2021-10-28 17:47:44 -0700

    first draft rrt:wq

--- chessWorld/search.py
+++ chessWorld/search.py
@@ -1,6 +1,7 @@
 import collections
 import heapq 
 import math
+import random
 
 class Search:
     def __init__(self, target, N=8, obstacles=[]):
@@ -23,12 +24,13 @@ class Search:
         actions = [[-2, 1], [-2, -1], [-1, 2], [-1, -2],
                         [2, 1], [2, -1], [1, 2], [1, -2]]
         stateSpace = [(i,j) for i in range(N) for j in range(N)]
-
+        obstacles = self.obstacles
         graph = collections.defaultdict(list)
+        
         for state in stateSpace:
             for action in actions:
                 nextState = (action[0]+state[0], action[1] + state[1])
-                if nextState in stateSpace:
+                if nextState in stateSpace and state not in obstacles and nextState not in obstacles:
                     graph[state].append(nextState)
         return graph
 
@@ -38,12 +40,11 @@ class Search:
         """
         graph = self.graph
         target = self.target
-        obstacles = self.obstacles
 
         if state == target:
             return [target]
         
-        if state in graph.keys() and state not in visited and state not in obstacles:
+        if state in graph.keys() and state not in visited:
             visited.add(state)
             for nextState in graph[state]:
                 feedback = self.dfs(nextState, visited)
@@ -58,7 +59,6 @@ class Search:
         graph = self.graph
         target = self.target
         stack = [[state, []]]
-        obstacles = self.obstacles
         visited = set()
 
         while stack:
@@ -67,7 +67,7 @@ class Search:
                 curState, curPath = stack.pop(0)
                 if curState == target:
                     return curPath + [curState]
-                if curState not in visited and curState not in obstacles:
+                if curState not in visited and curState:
                     visited.add(curState)
                     for nextState in graph[curState]:
                         temp.append([nextState, curPath + [curState]])
@@ -82,7 +82,6 @@ class Search:
             return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])
 
         graph = self.graph
-        obstacles = self.obstacles
         target = self.target
 
         open = [(0, state, [])]
@@ -95,16 +94,41 @@ class Search:
                 return path + [state]
             
             for nextState in graph[state]:
-                if nextState not in closed and nextState not in obstacles:
+                if nextState not in closed and nextState:
                     heapq.heappush(open, (calcDistance(nextState, target), nextState, path + [state]))                    
 
             closed.append(state)
         return []
 
 
+    def rrt(self, state):
+        def calcDistance(s1, s2):
+            return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])
+        
+        graph = self.graph
+        target = self.target
+        G = {'V': set((state,)), 'E': set()}
+        
+        while True:
+            state_random = random.choice(list(graph.keys()))
+            if state_random in graph.keys():
+                state_nearest = min([(s,calcDistance(s, state_random)) for s in G['V']], key=lambda x:x[1])[0] # one-liner
+                G['V'].add((state_random))
+                G['E'].add((state_nearest, state_random))
+            
+            if state_random == target:
+                newGraph = collections.defaultdict(list)
+                TODO
+                for state in G['E']:
+                    newGraph[state[0]].append(state[1])
+                    newGraph[state[1]].append(state[0])
+                    self.graph = newGraph
+                
+                return self.dfs(state)
+
 if __name__ == '__main__':
     target = (0,0)
-    obstacles=[(3,2)]
+    obstacles=[(1,3)]
     search = Search(target, 8, obstacles=obstacles)
-    res = search.Astar((5,3))
+    res = search.rrt((5,2))
     print(res)
