e5e81016556fd45194883bf8f583ba123b8f26d8
chokyzhou <nobreakfast@foxmail.com>
2021-11-02 00:44:53 -0700

    added documentation

---
+++ chessWorld/README.md
@@ -0,0 +1,23 @@
+# Search Algorithm for Chess World
+
+
+## Overview:
+This implements a knight's movement on a 8x8 chess game using DFS, BFS, A* and RRT. The program will output a valid path when given an initial state and a goal state.
+
+
+## How to run?
+```
+python search.py [serach method] [initial state] [goal state] [obstacle space]
+```
+
+The program can be easily run by using command above. Additional input methods are listed below:
+
+Search Method: DFS, BFS, Astar, rrt
+
+Initial State: Tuple
+
+Goal State: Tuple
+
+Obstacle Space: List[Tuple]
+
+
--- chessWorld/search.py
+++ chessWorld/search.py
@@ -2,6 +2,7 @@ import collections
 import heapq 
 import math
 import random
+import sys 
 
 class Search:
     def __init__(self, target, N=8, obstacles=[]):
@@ -102,28 +103,78 @@ class Search:
 
 
     def rrt(self, state):
+        """
+        rrt implementation
+        """
         def calcDistance(s1, s2):
+            """
+            p1 norm distance between states
+            """
             return abs(s1[0] - s2[0]) + abs(s1[1] - s2[1])
         
+        def isValidPath(s1, s2):
+            """
+            Determines if knight can go from s1 to s2
+            """
+            if abs(s1[0] - s2[0]) == 1 and abs(s1[1] - s2[1]) == 2:
+                return True
+            if abs(s1[0] - s2[0]) == 2 and abs(s1[1] - s2[1]) == 1:
+                return True
+            return False
+
         graph = self.graph
         target = self.target
         G = {'V': set((state,)), 'E': collections.defaultdict(set)}
         
         while True:
             state_random = random.choice(list(graph.keys()))
-            state_nearest = min([(s,calcDistance(s, state_random)) for s in G['V']], key=lambda x:x[1])[0] # one-liner
-            if calcDistance(state_random, state_nearest) <= calcDistance((0,0), (2,1)):
-                G['V'].add((state_random))
-                G['E'][state_nearest].add(state_random)
-                G['E'][state_random].add(state_nearest)
+            state_sorted = sorted([(s,calcDistance(s, state_random)) for s in G['V']], key=lambda x:x[1]) # one-liner
+            state_sorted = [(None, float('inf'))] + state_sorted
+
+            for i in range(1, len(state_sorted)):
+                state_nearest, dis, pre_dis = state_sorted[i][0], state_sorted[i][1], state_sorted[i-1][1]
+                
+                if dis <= pre_dis:
+                    if calcDistance(state_random, state_nearest) <= calcDistance((0,0), (2,1)) and isValidPath(state_random, state_nearest):
+                        G['V'].add((state_random))
+                        G['E'][state_nearest].add(state_random)
+                        G['E'][state_random].add(state_nearest)
+                
+                    if target in G['V']:
+                        self.graph = G['E']
+                        return self.dfs(state)
+
+
+def main(args):
+    try:
+        search_method = args[0]
+        target = args[1] # Goal state
+        ini = args[2] # Initial state
+        obstacles = args[3] # obstacle space
+    except:
+        search_method = "rrt"
+        target = (0,0) # Goal state
+        ini = (5,3) # Initial state
+        obstacles = [] # obstacle space
+        print("Missing args. Using default instead.")
+    
         
-            if target in G['V']:
-                self.graph = G['E']
-                return self.dfs(state)
-            
-if __name__ == '__main__':
-    target = (0,0)
-    obstacles=[(1,3)]
     search = Search(target, 8, obstacles=obstacles)
-    res = search.rrt((5,2))
-    print(res)
+
+    # select search method
+    if search_method == "dfs":
+        res = search.dfs(ini) 
+    if search_method == "bfs":
+        res = search.bfs(ini) 
+    if search_method == "a*":
+        res = search.Astar(ini) 
+    if search_method == "rrt":
+        res = search.rrt(ini) 
+
+    try:
+        print("A valid path: " + str(res))
+    except:
+        print("Search method not specified.")
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
