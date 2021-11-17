f2acb887fdeebc2092d60853b0327d21172dff08
choky <shenmeguislb@gmail.com>
2021-11-16 17:29:10 -0800

    added TODO

--- project/scripts/src/Qlearning.py
+++ project/scripts/src/Qlearning.py
@@ -5,11 +5,10 @@ import collections
 import math
 
 ### Initialize Q-learning
-discount_factor = 0.6
 epsilon = 0.2
 num_episodes = 10**6
 
-lr = .7
+lr = .8
 gamma = .95
 eps = 0.02
 ###
@@ -18,6 +17,10 @@ def obs2state(x, y, multiplier=100):
     return (int(math.floor(multiplier*x)), int(math.floor(multiplier*y))) 
 
 def main():
+    """
+    Obs: horizontal distance to the next pipe;
+         difference between the player's y position and the next hole's y position.
+    """
     
     env = flappy_bird_gym.make("FlappyBird-v0")
     Q = collections.defaultdict(lambda: np.zeros(env.action_space.n))
@@ -38,12 +41,13 @@ def main():
             total_reward += reward
             
             next_state = obs2state(*next_obs)
-            Q[state][action] += lr * (reward + gamma *  np.max(Q[next_state]) - Q[state][action])
+            Q[state][action] = Q[state][action] + lr * (reward + gamma *  np.max(Q[next_state]) - Q[state][action])
             
-            env.render()
-            time.sleep(1 / 100000)  # FPS
+            #env.render()
+            #time.sleep(1 / 60)  # FPS
 
             if done:
+                Q[state][action] = Q[state][action] + lr * (-100 + gamma *  np.max(Q[next_state]) - Q[state][action])
                 break
             
             state = next_state
