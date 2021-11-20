b94d45d365c4b9408802711da675537a49443f72
chokyzhou <nobreakfast@foxmail.com>
2021-11-20 03:23:09 -0800

    bird pass first pipe

--- project/scripts/README.md
+++ project/scripts/README.md
@@ -7,4 +7,8 @@ PLE https://pygame-learning-environment.readthedocs.io/en/latest/index.html
 
 Flappy-bird gym https://github.com/Talendar/flappy-bird-gym
 
+related paper:
+1.
+https://arxiv.org/pdf/2003.09579.pdf
+https://github.com/taivu1998/FlapAI-Bird
 # Usage
--- project/scripts/src/Qlearning.py
+++ project/scripts/src/Qlearning.py
@@ -4,101 +4,83 @@ import numpy as np
 import collections
 import math
 
-### Initialize Q-learning
-epsilon = 0.1
-num_episodes = 10**10
-
-lr = .8
-gamma = .95
-eps = 0.02
-###
-
-def create_bins_and_q_table(env):
-	# env.observation_space.high
-	# [4.8000002e+00 3.4028235e+38 4.1887903e-01 3.4028235e+38]
-	# env.observation_space.low
-	# [-4.8000002e+00 -3.4028235e+38 -4.1887903e-01 -3.4028235e+38]
-
-	# remove hard coded Values when I know how to
-
-	numBins = 20
-	obsSpaceSize = len(env.observation_space.high)
-
-	# Get the size of each bucket
-	bins = [
-		np.linspace(-4.8, 4.8, numBins),
-		np.linspace(-4, 4, numBins),
-		np.linspace(-.418, .418, numBins),
-		np.linspace(-4, 4, numBins)
-	]
-
-	qTable = np.random.uniform(low=-2, high=0, size=([numBins] * obsSpaceSize + [env.action_space.n]))
-	return bins, obsSpaceSize, qTable
-
-
-# Given a state of the enviroment, return its descreteState index in qTable
-def get_discrete_state(state, bins, obsSpaceSize):
-	stateIndex = []
-	for i in range(obsSpaceSize):
-		stateIndex.append(np.digitize(state[i], bins[i]) - 1) # -1 will turn bin into index
-	return tuple(stateIndex)
-
-def obs2state(a, b, multiplier=10000):
-    return (int(math.ceil(a*multiplier)), int(math.ceil(b*multiplier)))
-
-
-def main():
-    """
-    Obs: horizontal distance to the next pipe;
-         difference between the player's y position and the next hole's y position.
-    """
+class QLearning:
     
-    env = flappy_bird_gym.make("FlappyBird-v0")
-    bins, obsSpaceSize, qTable = create_bins_and_q_table(env)
-    
-    Q = collections.defaultdict(lambda: np.zeros(env.action_space.n))
-    
-    for episode in range(num_episodes): 
 
-        obs = env.reset()
-        #state = get_discrete_state(obs, bins, obsSpaceSize)
-        state = obs2state(*obs)
-        done = False
+    def __init__(self) -> None:
+        self.main()
+    
+    def obs2state(self,a, b, multiplier=100):
+        return (int(math.ceil(a*multiplier)), int(math.ceil(b*multiplier)))
+
+    def act(self, env, cur_epsilon, Q, state):
+        if np.random.uniform(0, 1) < cur_epsilon:
+            action = env.action_space.sample() # Explore action space
+        else:
+            action = np.argmax(Q[state]) # Exploit learned values
+        return action
+    
+    def main(self,):
+        """
+        Obs: horizontal distance to the next pipe;
+            difference between the player's y position and the next hole's y position.
+        """
+        ### Initialize Q-learning
+        initialEpsilon = 0.1
+        num_episodes = 10**10
+
+        lr = .9
+        gamma = .95
+        ###
         
-        cnt = 0
-        old_score = 0
-        while not done:
-            cnt += 1
-            if np.random.uniform(0, 1) < epsilon:
-                action = env.action_space.sample() # Explore action space
-            else:
-                action = np.argmax(Q[state]) # Exploit learned values
+        env = flappy_bird_gym.make("FlappyBird-v0")
+        
+        env.observation_space.sample
+        
+        Q = collections.defaultdict(lambda: np.zeros(env.action_space.n))
+        
+        for episode in range(num_episodes): 
 
-            next_obs, reward, done, info = env.step(action)
-            
-            #next_state = get_discrete_state(next_obs, bins, obsSpaceSize)
-            next_state = obs2state(*next_obs)
-            
-            if done or not -10 < next_state[1] < 10:
-                reward = -10000
-            
-            if info['score'] > old_score:
-                old_score = info['score']
-                reward = 100
-            else:
-                reward = 0
+            obs = env.reset()
+            #state = get_discrete_state(obs, bins, obsSpaceSize)
+            state = self.obs2state(*obs)
+            done = False
+            gameIter = []
+
+            epsilon = initialEpsilon / (episode + 1) 
+            #epsilon = initialEpsilon
+                           
+            while not done:
                 
-            Q[state][action] = (1 - lr) * Q[state][action] + lr * (reward + gamma * np.max(Q[next_state]))
+                action = self.act(env, epsilon, Q, state)
+
+                next_obs, reward, done, info = env.step(action)
+                
+                #next_state = get_discrete_state(next_obs, bins, obsSpaceSize)
+                next_state = self.obs2state(*next_obs)
+                
+                if done:
+                    reward = -10000
+                
+                if info['score']:
+                    reward = 100 * info['score']
+                
+                gameIter.append((state, action, reward, next_state))
+                
+                if episode % 5000 == 0:
+                    env.render()
+                    time.sleep(1 / 60)  # FPS
+                
+                state = next_state
             
-            if episode % 5000 == 0:
-                env.render()
-                time.sleep(1 / 60)  # FPS
+            for (state, action, reward, next_state) in gameIter:
+                Q[state][action] = (1 - lr) * Q[state][action] + lr * (reward + gamma * np.max(Q[next_state]))
+
+            #lr *= (episode+1) / (episode+2)
+            print(episode, info)
             
-            state = next_state
-            
-        print(episode, info)
-        
-    env.close()
+        env.close()
 
 if __name__ == "__main__":
-    main()
+    agent = QLearning()
+    agent.main()
